fc24570e324fd782e7ab4d78d55750650eb2c245
nmnhi <minhnhi08061105@gmail.com>
2024-07-08 00:31:27 +0700

    setup navigation and setup local stograge

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,10 +1,16 @@
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {loginStyles} from './styles';
 
 const LoginScreen = () => {
   return (
-    <View>
+    <View style={loginStyles.container}>
       <Text>LoginScreen</Text>
+      <Button
+        title="Login"
+        onPress={async () => await AsyncStorage.setItem('assetToken', 'token')}
+      />
     </View>
   );
 };
--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,10 +1,13 @@
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import React from 'react';
+import {homeStyles} from './styles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const HomeScreen = () => {
   return (
-    <View>
+    <View style={homeStyles.container}>
       <Text>HomeScreen</Text>
+      <Button title="Logout" onPress={async () => await AsyncStorage.clear()} />
     </View>
   );
 };
