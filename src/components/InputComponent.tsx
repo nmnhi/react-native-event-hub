bdf1be2c1ba1b7ddf6bdec492fee86064a5e83c6
nmnhi <minhnhi08061105@gmail.com>
2024-07-09 16:40:15 +0700

    Implement input custom component

---
+++ src/components/InputComponent.tsx
@@ -0,0 +1,100 @@
+import {
+  View,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  KeyboardType,
+} from 'react-native';
+import React, {ReactNode, useState} from 'react';
+import {appColors} from '../constants/appColors';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import {globalStyles} from '../styles/globalStyles';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+interface Props {
+  value: string;
+  onChange: (value: string) => void;
+  affix?: ReactNode;
+  placeholder?: string;
+  suffix?: ReactNode;
+  isPassword?: boolean;
+  allowClear?: boolean;
+  type?: KeyboardType;
+}
+
+const InputComponent = (props: Props) => {
+  const {
+    value,
+    onChange,
+    affix,
+    suffix,
+    placeholder,
+    isPassword,
+    allowClear,
+    type,
+  } = props;
+
+  const [isShowPassword, setIsShowPassword] = useState(isPassword ?? false);
+
+  return (
+    <View style={[styles.inputContainer]}>
+      {affix ?? affix}
+
+      <TextInput
+        style={[styles.input, globalStyles.text]}
+        value={value}
+        placeholder={placeholder ?? ''}
+        onChangeText={val => onChange(val)}
+        secureTextEntry={isShowPassword}
+        placeholderTextColor={'#747688'}
+        keyboardType={type ?? 'default'}
+      />
+
+      {suffix ?? suffix}
+
+      <TouchableOpacity
+        onPress={
+          isPassword
+            ? () => setIsShowPassword(!isShowPassword)
+            : () => onChange('')
+        }>
+        {isPassword ? (
+          <FontAwesome
+            name={isShowPassword ? 'eye-slash' : 'eye'}
+            size={22}
+            color={appColors.gray}
+          />
+        ) : (
+          value.length > 0 &&
+          allowClear && (
+            <AntDesign name="close" size={22} color={appColors.gray} />
+          )
+        )}
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default InputComponent;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: 'row',
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: appColors.gray3,
+    width: '100%',
+    minHeight: 56,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    backgroundColor: appColors.white,
+    marginBottom: 19,
+  },
+  input: {
+    padding: 0,
+    margin: 0,
+    flex: 1,
+    paddingHorizontal: 14,
+  },
+});
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,22 +1,34 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View} from 'react-native';
+import React, {useState} from 'react';
 
-import {authStyles} from './styles';
-import {ButtonComponent} from '../../components';
+import {InputComponent} from '../../components';
+import {globalStyles} from '../../styles/globalStyles';
+import {Lock, Sms} from 'iconsax-react-native';
+import {appColors} from '../../constants/appColors';
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View style={authStyles.container}>
-      <Text>LoginScreen</Text>
-      <ButtonComponent
-        text="Login"
-        type="link"
-        onPress={() => console.log('Login')}
-        icon={
-          <View>
-            <Text>N</Text>
-          </View>
-        }
+    <View
+      style={[
+        globalStyles.container,
+        {alignItems: 'center', justifyContent: 'center', padding: 20},
+      ]}>
+      <InputComponent
+        value={email}
+        onChange={value => setEmail(value)}
+        placeholder="Email"
+        affix={<Sms size={22} color={appColors.gray} />}
+        allowClear
+      />
+      <InputComponent
+        value={password}
+        onChange={value => setPassword(value)}
+        placeholder="Password"
+        affix={<Lock size={22} color={appColors.gray} />}
+        isPassword
       />
     </View>
   );
