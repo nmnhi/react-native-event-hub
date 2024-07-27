28eea323acb282c9d47e203a8b6655c23096ce5f
nmnhi <minhnhi08061105@gmail.com>
2024-07-28 01:30:00 +0700

    feature: Integrate API forgot password

--- src/screens/auth/ForgotPassword.tsx
+++ src/screens/auth/ForgotPassword.tsx
@@ -1,5 +1,8 @@
+import {useNavigation} from '@react-navigation/native';
 import {ArrowRight, Sms} from 'iconsax-react-native';
 import React, {useState} from 'react';
+import {Alert, View} from 'react-native';
+import authenticationAPI from '../../apis/authApi';
 import {
   ButtonComponent,
   ContainerComponent,
@@ -9,11 +12,44 @@ import {
   TextComponent
 } from '../../components';
 import {appColors} from '../../constants/appColors';
+import {LoadingModal} from '../../modals';
+import {globalStyles} from '../../styles/globalStyles';
+import {Validate} from '../../utils/validate';
 
-const ForgotPassword = () => {
+const ForgotPassword = ({navigation}: any) => {
   const [email, setEmail] = useState('');
+  const [isDisable, setIsDisable] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleCheckEmail = () => {
+    const isValidEmail = Validate.Email(email);
+    setIsDisable(!isValidEmail);
+  };
+
+  const handleForgotPassword = async () => {
+    setIsLoading(true);
+    const api = '/forgotPassword';
+    try {
+      const res: any = await authenticationAPI.HandleAuthentication(
+        api,
+        {email: email},
+        'post'
+      );
+      setIsLoading(false);
+      Alert.alert('Reset password', res.message, [
+        {
+          text: 'OK',
+          onPress: () => navigation.navigate('LoginScreen')
+        }
+      ]);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <ContainerComponent back isImageBackground>
+    <ContainerComponent back isImageBackground isScroll>
       <SectionComponent>
         <TextComponent
           text="Reset Password"
@@ -28,16 +64,32 @@ const ForgotPassword = () => {
           placeholder="abc@email.com"
           affix={<Sms size={22} color={appColors.gray} />}
           allowClear
+          onEnd={handleCheckEmail}
         />
       </SectionComponent>
       <SectionComponent>
         <ButtonComponent
+          disable={isDisable}
           text="SEND"
           type="primary"
-          icon={<ArrowRight size={20} color={appColors.white} />}
+          icon={
+            <View
+              style={[
+                globalStyles.iconContainer,
+                {
+                  backgroundColor: isDisable
+                    ? appColors.gray4
+                    : appColors.iconBackgroundColor
+                }
+              ]}>
+              <ArrowRight size={18} color={appColors.white} />
+            </View>
+          }
           iconFlex="right"
+          onPress={handleForgotPassword}
         />
       </SectionComponent>
+      <LoadingModal visible={isLoading} />
     </ContainerComponent>
   );
 };
